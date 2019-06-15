2594c591a4f9b7fef1d53cb83af2cbc7ef9812e7
SureshJoe <47621782+SureshJoe@users.noreply.github.com>
2019-06-15 11:57:04 +0530

    Add files via upload

--- Frontend/WebContent/RouteConfig.js
+++ Frontend/WebContent/RouteConfig.js
@@ -1,8 +1,27 @@
-var myApp=angular.module("myApp",['ngRoute']);
+var myApp=angular.module("myApp",['ngRoute','ngCookies']);
 myApp.config(function($routeProvider){
 	
 	alert("i am in Route Provider");
 	
-	$routeProvider.when("/login",{templateUrl:"Userpages/Login.html"})
-	              .when("/register",{templateUrl:"Userpages/Register.html"})
+	$routeProvider.when("/login",{templateUrl:"c_userpage/Login.html"})
+	              .when("/register",{templateUrl:"c_userpage/Register.html"})
+	              
+	              
+	              .when("/addBlog",{templateUrl:"c_blog/AddBlog.html"})
+                  .when("/manageBlog",{templateUrl:"c_blog/ManageBlog.html"})
+                  .when("/showBlog",{templateUrl:"c_blog/ShowBlog.html"})
+                  .when("/updateBlog",{templateUrl:"c_blog/UpdateBlog.html"})
+                  .when("/myBlog",{templateUrl:"c_blog/MyBlog.html"})
+});
+
+
+myApp.run(function($rootScope,$cookieStore)
+{
+		console.log('I am in bestrun Function');
+
+if($rootScope.currentUser==undefined)
+	{
+	$rootScope.currentUser=$cookieStore.get('userDetail');
+	}
+
 });
---
+++ Frontend/WebContent/c_userpage/UserController.js
@@ -0,0 +1,53 @@
+myApp.controller("UserController", function($scope,$location,$rootScope,$http,$cookieStore) 
+		{
+	$scope.userDetail={username:'', firstname:'', surname:'', password:'', emailId:'', role:'', status:'', isOnline:''};
+	
+	$rootScope.currentUser;
+	
+    $scope.checkUser=function() {
+    	
+    	$http.post('http://localhost:8080/Middleware/checkUser',$scope.userDetail)
+	      .then(function(response) {
+	    	  console.log("User checked");
+	    	  console.log(response.statusCode);
+	    	  $rootScope.currentUser=response.data;
+	    	  $cookieStore.put('userDetail',response.data);
+	    	  console.log($rootScope.currentUser);
+	    	  $location.path("/showBlog");
+	      },
+	      function(errresponse) {
+	    	  $scope.error="Username or password is wrong";
+	    	  console.log("Error checking user");
+	    	  $location.path("/login");
+	      });
+      }
+   
+$scope.addUser= function() {
+	  $scope.userDetail.role="ROLE_USER";
+	  $scope.userDetail.status='A';
+    $scope.userDetail.isOnline='Y';
+    
+    $http.post('http://localhost:8080/Middleware/addUser',$scope.userDetail)
+    .then(function(response) {
+  	  console.log("User added");
+  	  console.log(response.data);
+  	  $location.path("/login");
+    }, 
+    function(errresponse) {
+  	  $scope.error="Error in Registering";
+  	  console.log("Error adding user");
+  	  console.log(errresponse.data);
+  	  $location.path("/register");
+   });
+ }
+
+$scope.logout=function()
+{
+console.log("Logging out");
+delete $rootScope.currentUser;
+$cookieStore.remove('userDetail');
+alert("Logged out");
+$location.path("/login");
+}
+
+});
